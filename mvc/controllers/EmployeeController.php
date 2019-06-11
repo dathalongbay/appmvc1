c28287d141e25f2f57ae5ef1ea9f476c6d0960d3
dathalongbay <dathalongbay@gmail.com>
2019-06-11 19:04:42 +0700

    111

--- mvc/controllers/EmployeeController.php
+++ mvc/controllers/EmployeeController.php
@@ -3,4 +3,22 @@ namespace MVC\Controllers;
 
 class EmployeeController {
 
+
+    public function index() {
+
+    }
+
+
+    public function create() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function delete() {
+
+    }
+
 }
--- mvc/route.php
+++ mvc/route.php
@@ -7,7 +7,55 @@
  */
 namespace MVC;
 
+// nạp namespace
+use MVC\Controllers\EmployeeController;
+use MVC\Controllers\ErrorController;
+
 class Route {
 
+
+    public function run() {
+        /**
+         * url : index.php?controller=employee&action=index
+         * $controller = new EmployeeController();
+         * $controller->index();
+         *
+         * url : index.php?controller=employee&action=edit
+         * $controller = new EmployeeController();
+         * $controller->edit();
+         *
+         * url : index.php?controller=employee&action=delete
+         * $controller = new EmployeeController();
+         * $controller->delete();
+         *
+         * url : index.php?controller=employee&action=create
+         * $controller = new EmployeeController();
+         * $controller->create();
+         * $_REQUEST = $_POST + $_GET
+         */
+
+        if ($_REQUEST["controller"] == "employee") {
+            $controller = new EmployeeController();
+            if ($_REQUEST["action"] == "index") {
+                $controller->index();
+            }
+
+            if ($_REQUEST["action"] == "create") {
+                $controller->create();
+            }
+
+            if ($_REQUEST["action"] == "edit") {
+                $controller->edit();
+            }
+
+            if ($_REQUEST["action"] == "delete") {
+                $controller->delete();
+            }
+
+        }
+
+
+
+    }
 
 }
