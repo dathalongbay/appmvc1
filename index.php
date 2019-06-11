3d818ddcfc40a888f31c859c1f2dfd519a2612e0
dathalongbay <dathalongbay@gmail.com>
2019-06-11 18:53:55 +0700

    111

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-
+/**
+ * Nạp các file cần thiết vào luồng chạy của MVC
+ */
+include_once "mvc/Route.php";
+include_once "mvc/models/Database.php";
+include_once "mvc/controllers/EmployeeController.php";
+include_once "mvc/controllers/ErrorController.php";
+include_once "mvc/models/EmployeeModel.php";
 
 
