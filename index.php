f2108e66b3670520b10b70f625a3e605b3bf695b
dathalongbay <dathalongbay@gmail.com>
2019-06-11 18:54:58 +0700

    111

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Nạp các file cần thiết vào luồng chạy của MVC
  */
-include_once "mvc/Route.php";
+include_once "mvc/route.php";
 include_once "mvc/models/Database.php";
 include_once "mvc/controllers/EmployeeController.php";
 include_once "mvc/controllers/ErrorController.php";
